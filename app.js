3da51b49a7c255e2a357991ff58125920d388eea
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2019-04-14 23:03:08 -0700

    add mongodb & mongoose

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const config = require('./config/database');
+
+mongoose.connect(config.database, { useNewUrlParser: true });
+let db = mongoose.connection;
+
+// connect to MongoDB
+db.once('open', function() {
+  console.log('Connected to MongoDB');
+});
+
+// check for MongoDB connection error
+db.on('error', function(err) {
+  console.log(err);
+});
 
 const app = express();
 const port = 3000;
